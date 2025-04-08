8f6b868b197c1fe946ebcae6dcb2ede33e991506
ann <123750949+annnabui@users.noreply.github.com>
2025-04-08 01:01:47 -0400

    2

---
+++ sketch.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const disappear = document.getElementById("img-cont");
+  
+    disappear.addEventListener("click", () => {
+        disappear.style.display = "none";
+    });
+});
+  
